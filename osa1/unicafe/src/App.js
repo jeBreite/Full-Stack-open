abc970fc10e35fb9950442e8825d2f2a68c48a7f
Jenni Breite <jenni.breite@edu.karelia.fi>
2023-03-24 14:08:39 +0200

    step2 & step 3

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,5 +1,23 @@
 import { useState } from "react";
 
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = () => good + neutral + bad;
+  const average = () => [good + bad * -1] / all();
+  const positive = () => (good * 100) / all();
+  return (
+    <div>
+      {" "}
+      <p>all {all()}</p>
+      <p>average {average()}</p>
+      <p>positive {positive()}</p>
+    </div>
+  );
+};
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0);
@@ -9,13 +27,14 @@ const App = () => {
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <h1>Statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
