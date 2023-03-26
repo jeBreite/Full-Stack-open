fc8ddc4d184ce4a1c7729aef440a30a000eb72b7
Jenni Breite <jenni.breite@edu.karelia.fi>
2023-03-26 13:40:38 +0300

    1.10: unicafe step5

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,39 +1,66 @@
 import { useState } from "react";
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>{props.text}</button>
-);
+// Button-komponentti, joka vastaanottaa onClick- ja text-propsit
+// ja renderöi napin, jonka teksti on text-propsin arvo ja jonka klikkauksesta
+// kutsutaan onClick-propsin funktiota
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
-const Statistics = ({ good, neutral, bad }) => {
-  const all = () => good + neutral + bad;
-  const average = () => [good + bad * -1] / all();
-  const positive = () => (good * 100) / all();
+// StatisticLine-komponentti, joka vastaanottaa text- ja value-propsit
+// ja renderöi tekstirivin, jossa on text-propsin ja value-propsin arvot
+const StatisticLine = ({ text, value }) => {
   return (
     <div>
-      {" "}
-      <p>all {all()}</p>
-      <p>average {average()}</p>
-      <p>positive {positive()}</p>
+      <p>
+        {text} {value}
+      </p>
     </div>
   );
 };
 
+// Luodaan Statistics-komponentti, joka vastaanottaa good-, neutral- ja bad-propsit
+// ja renderöi tilastot
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = [good + bad * -1] / all;
+  const positive = (good * 100) / all + " %";
+
+  if (all !== 0) {
+    return (
+      <div>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive} />
+      </div>
+    );
+    // Jos palautteita ei ole vielä annettu, näytetään teksti "No feedback given"
+  } else {
+    return <div>No feedback given</div>;
+  }
+};
+
+// App-komponentti, joka renderöi napit ja tilastot
 const App = () => {
-  // tallenna napit omaan tilaansa
+  // Luodaan tilamuuttujat good, neutral ja bad useState-hookin avulla
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  // Luodaan funktiot napin klikkauksen käsittelyä varten
+  const handleGoodClick = () => setGood(good + 1);
+  const handleNeutralClick = () => setNeutral(neutral + 1);
+  const handleBadClick = () => setBad(bad + 1);
+
+  // Renderöidään napit ja tilastot
   return (
     <div>
       <h1>Give feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Button onClick={handleGoodClick} text="good" />
+      <Button onClick={handleNeutralClick} text="neutral" />
+      <Button onClick={handleBadClick} text="bad" />
       <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
